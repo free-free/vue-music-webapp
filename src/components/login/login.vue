755374ebf9a2c2b8d1dd1c2e560d326370f9f197
jell <infinite.ft@gmail.com>
2019-04-11 11:25:32 +0800

    Add login and register component

---
+++ src/components/login/login.vue
@@ -0,0 +1,187 @@
+<template>
+<transition name="slide">
+  <div class="login">
+    <div class="background-mask">
+      <div class="container">
+          <el-form :model="loginData" :rules="loginFormRules" ref="loginForm" label-position="left" label-width="0px" class="ruleForm login-form" >
+            <h3 class="title">登录Cloud Music</h3>
+            <el-form-item prop="account" class="form-item">
+              <el-input type="text" v-model="loginData.account"  auto-complete="on" class="form-input" placeholder="用户名/邮箱" @keyup.enter.native="onLoginBtnClick" prefix-icon="fa fa-user-circle-o" size="medium"></el-input>
+            </el-form-item>
+            <el-form-item prop="password"  class="form-item">
+              <el-input type="password"
+              class="form-input" v-model="loginData.password"  auto-complete="on" placeholder="密码" @keyup.enter.native="onLoginBtnClick" prefix-icon="fa fa-lock"  size="medium"></el-input>
+            </el-form-item>
+            <el-form-item class="form-item">
+              <el-button type="primary" style="width:100%;background-color:#d44439;outline-color: #d44439;border-color: #d44439;" @click.native.prevent="onLoginBtnClick" :loading="logining">登录</el-button>
+            </el-form-item>
+            <div style="margin:10px auto;width:70%">
+              <span style="color: white;" @click="onResetPasswdBtnClick">忘记密码?</span>
+              <span style="float: right;color: white;" @click="onRegisterBtnClick">注册>></span>
+            </div>
+          </el-form>
+          
+      </div>
+  </div>
+</div>
+</transition>
+</template>
+
+<script>
+  import Auth from '../../api/auth';
+  export default {
+    data() {
+      return {
+        logining: false,
+        loginData: {
+          account: '',
+          password: ''
+        },
+        loginFormRules: {
+          account: [
+            { required: true, message: '请输入用户名/邮箱', trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+          ]
+        },
+        checked: true,
+        userName: '',
+        password: '',
+        isBtnLoading: false
+      }
+    },
+    created () {
+      if(JSON.parse(localStorage.getItem('user')) && JSON.parse( localStorage.getItem('user')).userName)
+      {
+        this.userName = JSON.parse(localStorage.getItem('user')).userName;
+        this.password = JSON.parse(localStorage.getItem('user')).password;
+      }
+    },
+    computed: {
+      btnText() {
+        if (this.isBtnLoading) return '登录中...';
+        return '登录';
+      }
+    },
+    methods: {
+      onResetPasswdBtnClick:function(){
+        console.log('reset password');
+      },
+      onRegisterBtnClick:function(){
+        this.$router.push({"path":'/register'});
+      },
+      onLoginBtnClick:function() {
+        var _this = this;
+        this.$refs.loginForm.validate(function(valid) {
+          if (valid) {
+            var data = {};
+            _this.logining = true;
+            if(_this.checked){
+              localStorage.setItem('account', _this.loginData['account']);
+              localStorage.setItem('password', _this.loginData['password']);
+            }
+            else{
+              localStorage.removeItem('account');
+              localStorage.removeItem('password');
+            }
+            data['account'] = _this.loginData['account'];
+            data['password'] = md5(_this.loginData['password']);
+            Auth.login(data, function(res){
+              _this.logining = false;
+              switch(res.errcode){
+                case 40001:
+                case 40002:
+                  _this.$message({
+                    message: "用户名或密码不正确!",
+                    type: 'error'
+                  });
+                break;
+                case 40003:
+                  _this.$message({
+                    message: "用户未激活, 请登录邮箱激活!",
+                    type: 'error'
+                  });  
+                break;
+                case 40004:
+                  _this.$message({
+                    message: "用户未注册!",
+                    type: 'error'
+                  });  
+                break;
+                default:
+                if (!res.errcode) {
+                  for (var key in res){
+                    sessionStorage.setItem(key, res[key]);
+                  }
+                  console.log(res);
+                  _this.$router.push({ path: '/' });
+                }
+                break;
+              }
+              
+            });
+          }
+        });
+      },
+    }
+  }
+</script>
+<style lang="scss" scoped >
+@import "~common/scss/variable";
+.login {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  background: $color-background;
+  background:url('../../assets/login-bg.jpg');
+  background-repeat:no-repeat;
+  background-position:0% 0%;
+  background-size:cover;
+  &.slide-enter-active, &.slide-leave-active {
+    transition: all 0.0s;
+  }
+  &.slide-enter, &.slide-leave-to {
+    transform: translate3d(-50%, 0, 0);
+    opacity: 0;
+  }
+  .container{
+    position:fixed;
+    top: 40%;
+    left:50%;
+    width:100%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    background-color: transparent;
+  }
+  .background-mask{
+    width:100%;
+    height:100vh;
+    background-color:#000;
+    background: rgba(0, 0, 0, 0.4);
+    box-sizing:border-box;
+    display:flex;justify-content:center;
+    align-items:center;
+  }
+  .login-form {
+    margin: 0px auto;
+    vertical-align:middle;
+    .title {
+      margin: 10px auto 40px auto;
+      text-align: center;
+      color: #fff;
+    }
+    .form-item{
+      margin-left: auto;
+      margin-right: auto;
+      margin-top:25px;
+      height: 50px;
+      width:70%;
+    }
+  }
+  
+}
+</style>
+
---
+++ src/components/register/register.vue
@@ -0,0 +1,190 @@
+<template>
+<transition name="slide">
+  <div class="register">
+    <div class="background-mask">
+      <div class="container">
+        <el-form :model="registerData" :rules="registerFormRules" ref="registerForm" label-position="left" label-width="0px" class="ruleForm register-form">
+          <h3 class="title">注册 CloudMusic</h3>
+          <el-form-item prop="username" class="form-item">
+            <el-input type="text" v-model="registerData.username"  auto-complete="on" placeholder="用户名" @keyup.enter.native="onRegisterBtnClick"  prefix-icon="fa fa-user" size="medium"></el-input>
+          </el-form-item>
+          <el-form-item prop="email" class="form-item">
+            <el-input type="text" v-model="registerData.email"  auto-complete="on" placeholder="邮箱" @keyup.enter.native="onRegisterBtnClick" prefix-icon="fa fa-envelope" size="medium">
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password"  class="form-item">
+            <el-input type="password" v-model="registerData.password"  auto-complete="on" placeholder="密码" @keyup.enter.native="onRegisterBtnClick" size="medium" prefix-icon="fa fa-lock"></el-input>
+          </el-form-item>
+          <el-form-item class="form-item">
+            <el-button style="width:100%;margin-top:10px;" type="success" @click.native.prevent="onRegisterBtnClick" :loading="registering">注册</el-button>
+          </el-form-item>
+          <div style="margin:10px auto;width:70%">
+            <span style="color: white;" @click="onBackLoginBtnClick"><<登录</span>
+          </div>
+        </el-form>
+      </div>
+  </div>
+</div>
+</transition>
+</template>
+
+<script>
+  import Auth from '../../api/auth';
+  export default {
+    data() {
+      return {
+        registering: false,
+        registerData:{
+          email:'',
+          username:'',
+          password:''
+        },
+        registerFormRules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min:6, max:30, message: '用户名长度不能小于6位', trigger: 'blur' },
+          ],
+          email: [
+            { required: true, message: '请输入邮箱', trigger: 'blur' },
+            { pattern:'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$', trigger:'blur', message: "请输入正确的邮箱"}
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min:6, max:30, message: '密码长度不能小于7位', trigger: 'blur' },
+          ]
+        },
+        checked: true,
+        userName: '',
+        password: '',
+        isBtnLoading: false
+      }
+    },
+    created () {
+      if(JSON.parse( localStorage.getItem('user')) && JSON.parse( localStorage.getItem('user')).userName){
+        this.userName = JSON.parse( localStorage.getItem('user')).userName;
+        this.password = JSON.parse( localStorage.getItem('user')).password;
+      }
+    },
+    computed: {
+      btnText() {
+        if (this.isBtnLoading) return '登录中...';
+        return '登录';
+      }
+    },
+    methods: {
+      onBackLoginBtnClick:function(){
+        this.$router.push({ path: '/login' })
+      },
+      onRegisterBtnClick:function(){
+        var _this = this;
+        this.$refs.registerForm.validate(function(valid) {
+          if (valid) {
+            var data = {};
+            _this.registering = true;
+            data['username'] = _this.registerData['username'];
+            data['password'] = md5(_this.registerData['password']);
+            data['email'] = _this.registerData['email'];
+            Auth.register(data, function(res){
+              _this.registering = false;
+              switch(res.errcode){
+                case 40005:
+                  _this.$message({
+                    message: "该邮箱已被注册过!",
+                    type: 'error'
+                  });
+                break;
+                case 50001:
+                  _this.$message({
+                    message: "用户注册失败,请重试!",
+                    type: 'error'
+                  });
+                break;
+                case 60001:
+                  _this.$message({
+                    message: "用户注册已打烊，请择日再来!",
+                    type: 'error'
+                  });
+                break;
+                default:
+                  if (!res.errcode) {
+                    localStorage.setItem('account', _this.registerData['email']);
+                    localStorage.setItem('password', _this.registerData['password']);
+                    _this.$message({
+                      message: "激活邮件已发送到你的邮箱，请去激活账号",
+                      type: 'info',
+                      center: true,
+                      duration: 2500,
+                      onClose:function(){
+                        _this.loginData.account = _this.registerData['email'];
+                        _this.loginData.password = _this.registerData['password'];
+                        _this.$refs.registerForm.resetFields();
+                        window.location.reload();
+                      }
+                    });
+                  }
+                break;
+              }    
+            });
+          }
+        });
+      },
+    }
+  }
+</script>
+<style lang="scss" scoped >
+@import "~common/scss/variable";
+.register {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  background: $color-background;
+  background:url('../../assets/login-bg.jpg');
+  background-repeat:no-repeat;
+  background-position:0% 0%;
+  background-size:cover;
+  &.slide-enter-active, &.slide-leave-active {
+    transition: all 0.0s;
+  }
+  &.slide-enter, &.slide-leave-to {
+    transform: translate3d(-50%, 0, 0);
+    opacity: 0;
+  }
+  .container{
+    position:fixed;
+    top: 40%;
+    left:50%;
+    width:100%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    background-color: transparent;
+  }
+  .background-mask{
+    width:100%;
+    height:100vh;
+    background-color:#000;
+    background: rgba(0, 0, 0, 0.4);
+    box-sizing:border-box;
+    display:flex;justify-content:center;
+    align-items:center;
+  }
+  .register-form {
+    margin: 0px auto;
+    vertical-align:middle;
+    .title {
+      margin: 10px auto 40px auto;
+      text-align: center;
+      color: #fff;
+    }
+    .form-item{
+      margin-left: auto;
+      margin-right: auto;
+      margin-top:10px;
+      height: 45px;
+      width:70%;
+    }
+  }
+  
+}
+</style>
