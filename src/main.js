2f802aaaa177e683486c5adcc73af31f61d0d02d
jell <infinite.ft@gmail.com>
2019-04-23 21:02:04 +0800

    Import css file of element-ui and comment out the authentication middleware

--- src/main.js
+++ src/main.js
@@ -1,22 +1,45 @@
-import Vue from 'vue'
+/*import Vue from 'vue'*/
 import App from './App'
 import router from './router'
 import store from './store'
 import fastclick from 'fastclick'
 import VueLazyload from 'vue-lazyload'
+import gallery from 'img-vuer';
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
 Vue.use(VueLazyload, {
   loading: require('common/image/default.png')
-})
+});
+Vue.use(gallery, {
+  swipeThreshold: 150
+});
 
 fastclick.attach(document.body)
 
+/*
+router.beforeEach(function(to, from, next){
+ 
+  //if (to.path == '/login') {
+  //  sessionStorage.clear();
+  //}
+
+  var user = sessionStorage.getItem('_id');
+  if (!user && to.path != '/login') {
+  	if (to.path != '/register'){
+  		next({ path: '/login' })	
+  	}else{
+  		next()
+  	}
+  } else {
+    next()
+  }
+})
+*/
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
   render: h => h(App)
-})
+}).$mount('#app')
